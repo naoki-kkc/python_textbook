06dbe23c5619abe0e75474ed2033acd2746e60f9
naoki-kkc <naoki2kuchi@gmail.com>
2022-09-13 00:27:19 +0900

    complete chapter 05

---
+++ chapter_05/p117.py
@@ -0,0 +1,4 @@
+open_file = open('./chapter_05/read_sample.txt')
+read_result = open_file.read()
+print(read_result)
+open_file.close()
---
+++ chapter_05/p121.py
@@ -0,0 +1,4 @@
+write_file = open('./chapter_05/write_sample.txt', 'w')
+write_file.write('this is write text.\n')
+write_file.write('this is second sentence.\n')
+write_file.close()
